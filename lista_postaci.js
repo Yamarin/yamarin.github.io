ec63f6143db46defc0ce9358b7e9fc9c188ffd7c
Yamarin <tigerland277@gmail.com>
2023-02-24 00:41:14 +0100

    Update lista_postaci.js

--- lista_postaci.js
+++ lista_postaci.js
@@ -5,10 +5,9 @@ const url = "https://api.airtable.com/v0/applHD7TN8d6YaVSF/Table%201"
   
 
   async function fetchText() {
-	const bearer = "'Bearer " + ${{secrets.AIRTABLE_API_KEY}} + "'"
     const response = await fetch(url, {
 		headers: {
-			'Authorization': bearer	}
+			'Authorization': 'Bearer ${{secrets.AIRTABLE_API_KEY}}'	}
 		}
 	); //ogolne zapytanie do tabeli z postaciami
 	
